f5697866db3b5e86df0ac6fda95758aed17322ae
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-10-22 20:25:36 +0900

    Exercise 2.2, 2.3

--- 2/2.1.rkt
+++ 2/2.1.rkt
@@ -15,6 +15,9 @@
 (define (numer x) (car x))
 (define (denom x) (cdr x))
 
+(define (average x y)
+  (/ (+ x y) 2))
+
 ; suppose we have procedures
 ; (make-rat n d) returns a rational numbers whose numerator n and denominator d
 ; (numer x) returns numerator of rational number x
@@ -59,3 +62,64 @@
 
 (define minus-one-half (make-rat -1 2))
 (print-rat minus-one-half)
+
+; Exercise 2.2
+(define (make-point x y)
+  (cons x y))
+(define (x-point p)
+  (car p))
+(define (y-point p)
+  (cdr p))
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point
+   (average (x-point (start-segment segment)) (x-point (end-segment segment)))
+   (average (y-point (start-segment segment)) (y-point (end-segment segment)))))
+
+(define (print-segment segment)
+  (newline)
+  (display "[")
+  (print-point (start-segment segment))
+  (display ",")
+  (print-point (end-segment segment))
+  (newline)
+  (display "]"))
+
+(define p1 (make-point 2 4))
+(define p2 (make-point 12 17))
+(define line-p (make-segment p1 p2))
+
+; (midpoint-segment line-p)
+
+
+; Exercise 2.3
+(define (make-rect x y z)
+  (list x y z))
+(define (first-point rect)
+  (car rect))
+(define (second-point rect)
+  (car (cdr rect)))
+(define (third-point rect)
+  (car (cdr (cdr rect))))
+
+(define p3 (make-point 0 0))
+
+(define rect-a (make-rect p1 p2 p3))
+(first-point rect-a)
+(second-point rect-a)
+(third-point rect-a)
+;; skip perimeter and area
