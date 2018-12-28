0eb430c3733891e32dab3edc10b7d141d411fd87
tkmct <1220t.takamichi@gmail.com>
2018-12-29 01:22:46 +0900

    ex 3.2

--- 3/3.1.rkt
+++ 3/3.1.rkt
@@ -4,3 +4,12 @@
 (define (make-accumulator i)
   (let ((sum i))
     (lambda (x) (begin (set! sum (+ sum x)) sum))))
+
+;; Ex3.2
+(define (make-monitored procedure)
+  (let ((ms 0))
+    (lambda (x)
+      (if (eq? x 'how-many-calls?)
+          ms
+          (begin (set! ms (+ ms 1))
+                 (procedure x))))))
